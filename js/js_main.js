5d218d582b1bc0326ae5c02bb9b84c06f0da5692
Jellrin <shukwaun.cheung@gmail.com>
2018-12-10 00:22:58 +0100

    neue navi - noch nicht funktionstücktig

--- js/js_main.js
+++ js/js_main.js
@@ -60,4 +60,11 @@ var Points = L.layerGroup();
 		window.open("https://www.sebastiansettgast.com/#" + e.latlng.lat +"/" + e.latlng.lng , "_blank");
 	}
 
-		mymap.on('click', onMapClick);
+	// Get the button, and when the user clicks on it, execute myFunction
+	document.getElementById("button").onclick = function() {myFunction()
+	};
+
+	/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+	function myFunction() {
+		document.getElementById("dropdown").classList.toggle("show");
+	}
